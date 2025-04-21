6c09e46d9ef342b205d71fc353201933dc8cf87a
April-lie-debug <57951600@qq.com>
2025-04-22 00:20:07 +0800

    Add files via upload

---
+++ hw2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from weather_fetcher import WeatherFetcher
+
+app = Flask(__name__)
+weather_fetcher = WeatherFetcher()
+
+
+@app.route('/')
+def index():
+    forecasts = weather_fetcher.get_weather_forecast("101220101")
+    aqi = weather_fetcher.get_aqi("101220101")
+    return render_template('index.html', forecasts=forecasts, aqi=aqi)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
